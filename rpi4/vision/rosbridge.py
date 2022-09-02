318f30e1bb1b5eff9314c9d4df550bf8e9d5c17d
leommiranda <leonardomichalskim@gmail.com>
2022-09-02 17:22:03 -0300

    first version not-working of the GazeboVideoSource

--- rpi4/vision/rosbridge.py
+++ rpi4/vision/rosbridge.py
@@ -32,13 +32,42 @@ class RosBridge(object):
         elif topic == topics.TOPIC_DONT_DETECT_ARUCO:
             self.pub_dont_detect_aruco.publish("Dont detect aruco")  # TODO: change to Boolean msg
             return
-        # elif topic == topics.TOPIC_IMAGE_ARRAY:
-        #     return
-        # elif topic == topics.TOPIC_IMAGE_JPEG:
-        #     return
 
     def start(self):
         rospy.init_node('rpi4_vision', anonymous=False, disable_signals=True)
 
     def stop(self):
-        rospy.signal_shutdown("CLOSING THE APP PROPERLY")
+        rospy.signal_shutdown("CLOSING ROS BRIDGE PROPERLY")
+
+
+class GazeboVideoSource(object):
+    def __init__(self):
+        self.camera_ros_node = rospy.Subscriber("??????", String, analyze)  # TODO
+        self.subscriber_list = []
+
+    def subscribe(self, subscriber):
+        self.subscriber_list.append(subscriber)
+
+    def publish(self, msg, topic):
+        for subscriber in self.subscriber_list:
+            subscriber.receive_msg(msg=msg, topic=topic)
+
+    def analyze(self, data):
+        img_jpeg = data.data  # TODO
+        img_array = data.data  # TODO
+        creation_time = 0  # TODO
+        self.publish(
+            msg=msgs.Image(image=img_jpeg, creation_time=creation_time),
+            topic=topics.TOPIC_IMAGE_JPEG
+        )
+        self.publish(
+            msg=msgs.Image(image=img_array, creation_time=creation_time),
+            topic=topics.TOPIC_IMAGE_ARRAY
+        )
+
+    # as the RosBridge node would be already running, we can't don't need to start another node
+    # def start(self):
+    #     rospy.init_node('gazebo_camera', anonymous=False, disable_signals=True)
+
+    # def stop(self):
+    #     rospy.signal_shutdown("CLOSING GAZEBO VIDEO SOURCE PROPERLY")
